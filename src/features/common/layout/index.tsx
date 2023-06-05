95e5e791b867772cf60c736cc5c408a7ca0cb197
Jimmy Wu <jimmy@wemakeapp.net>
2023-06-05 23:39:37 +0800

    feat: add main page background

--- src/features/common/layout/index.tsx
+++ src/features/common/layout/index.tsx
@@ -10,20 +10,37 @@ const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
   useOneSignal();
   const router = useRouter();
   const { route } = router;
-
+  const isMainPage = route === "/";
   return (
     <div>
       <HeadHtml />
-      <Navbar />
-      <main
-        className={cn(
-          "min-h-[78vh] ",
-          route !== "/location" && "container mx-auto"
+      <div className={cn(isMainPage && "relative h-screen overflow-hidden")}>
+        {isMainPage && (
+          <video
+            autoPlay
+            loop
+            muted
+            className="absolute -z-10 w-auto 
+            min-w-full min-h-full max-w-none opacity-50"
+          >
+            <source
+              // src="https://surpass-boxing-gym.s3.ap-southeast-1.amazonaws.com/videos/background_compressed.mp4"
+              src="https://surpass-boxing-gym.s3.ap-southeast-1.amazonaws.com/videos/background.mp4"
+              type="video/mp4"
+            />
+          </video>
         )}
-      >
-        {children}
-      </main>
-      <Footer />
+        <Navbar />
+        <main
+          className={cn(
+            "min-h-[78vh] ",
+            route !== "/location" && "container mx-auto"
+          )}
+        >
+          {children}
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 };
