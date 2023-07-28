ea13acbb33e42dc2d2469d573d367f6176cbd79b
Jimmy Wu <jimmy@wemakeapp.net>
2023-07-28 15:14:30 +0100

    feat: to hide class info if the user doesn't have lesson valid package

---
+++ src/features/classes/components/ClassContent.tsx
@@ -0,0 +1,117 @@
+import { Button, useDisclosure } from "@chakra-ui/react";
+
+import { endOfDay, format, isBefore, startOfDay, subDays } from "date-fns";
+import useTranslation from "next-translate/useTranslation";
+import { useState } from "react";
+
+import { OpenModelType } from "@/features/common/enums/OpenModelType";
+import { useSession } from "next-auth/react";
+
+import { ModalComponent } from "@/features/common/components/Modal";
+
+import { DatePicker } from "@/features/common/components/DatePicker";
+
+import { ClassesPageModalContent } from "@/features/classes/components/ClassesPageModalContent";
+import { ClassPageContent } from "@/features/classes/components/ClassPageContent";
+import { trpc } from "@/utils/trpc";
+import { useRouter } from "next/router";
+import { Lessons } from "@prisma/client";
+
+export const ClassContent = ({
+  lessonsData,
+}: {
+  lessonsData: Lessons[] | undefined;
+}) => {
+  const { t } = useTranslation("classes");
+  const session = useSession();
+  const router = useRouter();
+  const { date: queryDate } = router.query;
+  const now = new Date();
+
+  const noSpecificDate =
+    !queryDate ||
+    isNaN(new Date(queryDate.toString()) as unknown as number) ||
+    isBefore(new Date(queryDate.toString()), startOfDay(now));
+
+  const isAuthenticated = session.status === "authenticated";
+
+  const [modelType, setModelType] = useState(OpenModelType.OPEN_CLASS);
+
+  const modalDisclosure = useDisclosure();
+  const { onOpen } = modalDisclosure;
+
+  const handleOpenModel = () => {
+    onOpen();
+    if (!isAuthenticated) {
+      setModelType(OpenModelType.LOGIN);
+      return;
+    }
+    if (!lessonsData || lessonsData.length === 0) {
+      setModelType(OpenModelType.REQUEST_LESSON_MESSAGE);
+      return;
+    }
+    setModelType(OpenModelType.OPEN_CLASS);
+  };
+
+  const [query, setQuery] = useState({
+    skip: 0,
+    date: noSpecificDate ? now.toString() : queryDate.toString(),
+  });
+
+  const bookingTimeSlotResult =
+    trpc.bookingTimeSlotRouter.fetchForStudent.useQuery(query);
+  const { data, isLoading } = bookingTimeSlotResult;
+  const date = new Date(query.date);
+  const minDate = endOfDay(subDays(new Date(), 1));
+
+  return (
+    <>
+      <div className="space-y-2 p-2 md:p-0">
+        <h1 className="text-white text-3xl">{t("classes")}</h1>
+        <div className="flex space-x-2 items-center">
+          <div className="w-36">
+            <DatePicker
+              datePickerProps={{
+                date,
+                onDateChange: (value) => {
+                  setQuery(() => ({
+                    skip: 0,
+                    date: value.toString(),
+                  }));
+                },
+                minDate,
+              }}
+            />
+          </div>
+          <div className="text-white">
+            {t(format(date, "EEEE").toLowerCase())}
+          </div>
+          <div>
+            <Button onClick={handleOpenModel} isDisabled={data?.isDayOff}>
+              {t("open_a_class")}
+            </Button>
+          </div>
+        </div>
+        <ClassPageContent
+          data={data}
+          isLoading={isLoading}
+          lessonsData={lessonsData}
+          handleOpenModel={handleOpenModel}
+          query={query}
+          setQuery={setQuery}
+        />
+      </div>
+      <ModalComponent
+        modalDisclosure={modalDisclosure}
+        content={
+          <ClassesPageModalContent
+            modalDisclosure={modalDisclosure}
+            setModelType={setModelType}
+            modelType={modelType}
+            date={date}
+          />
+        }
+      />
+    </>
+  );
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
-// middleware.ts
 import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
-const RESTRICTED_COUNTRIES = ["GB"];
+
 export async function middleware(request: NextRequest, _next: NextFetchEvent) {
   const res = NextResponse.next();
   const country = request.geo?.country ?? "";
 
-  if (RESTRICTED_COUNTRIES.includes(country)) {
+  if (country === "GB" && request.url.includes("/classes")) {
+    return NextResponse.rewrite(new URL("/lesson", request.url));
+  }
+  if (country !== "GB" && request.url.includes("/lesson")) {
     return NextResponse.rewrite(new URL("/classes", request.url));
   }
   return res;
